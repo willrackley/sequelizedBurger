67f8091e876aa852fa0b92d914bb36a7d0e208e2
williamrackley <willrackley65@gmail.com>
2019-02-25 16:03:32 -0500

    connect app to mySQL database and created tabel using a model

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var PORT = process.env.PORT || 8000;
 var app = express();
 
+var db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -10,15 +12,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgersController.js");
-
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("Burger App listening on port: ", PORT);
+  });
 });
