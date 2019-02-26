91669bb0155f56eb0f9e6b8699c0d9660818b698
williamrackley <willrackley65@gmail.com>
2019-02-25 20:58:31 -0500

    started working on html, js files

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
+
+  var postedBurger;
+  // InitializeRows appends all of the burger informaiton that is entered to the screen
+  function initializeRows() {
+    $('#listedBurgers').empty();
+    var burgersToAdd = [];
+    for (var i = 0; i < posts.length; i++) {
+      burgersToAdd.push(createNewRow(postedBurger[i]));
+    }
+    $('#listedBurgers').append(burgersToAdd);
+  }
+
+  function createNewRow(postedBurger) {
+    var formattedDate = new Date(post.createdAt);
+    formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+    var newBurgerEntry = $("<div>");
+    newBurgerEntry.addClass = "border";
+
+  }
     
   $(".devour-form").on("submit", function(event) {
     event.preventDefault();
 
-    var burger_id = $(this).children(".burger_id").val();
-    console.log(burger_id);
-    $.ajax({
-      method: "PUT",
-      url: "/burgers/" + burger_id
-    }).then(function(data) {
-      // reload page to display devoured burger in proper column
-      location.reload();
-    });
+    
 
   });
 });
