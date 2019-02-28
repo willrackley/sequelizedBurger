5b25d53db1b7197cb59b61c257235a0c644ea867
williamrackley <willrackley65@gmail.com>
2019-02-28 14:41:24 -0500

    heroku deploy edit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 require("./routes/burgers-api-routes.js")(app);
 require("./routes/customer-api-routes.js")(app);
 
-
+// Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("Burger App listening on port: ", PORT);
